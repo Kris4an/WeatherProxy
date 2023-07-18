ff554a828bb7942148d019fa53b20abeeacfcbb1
YoanYovchev <yoyo.yovchev@gmail.com>
2023-07-18 15:33:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+require('dotenv').config()
+const rateLimit = require('express-rate-limit')
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 12
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+app.use('/current', require('./routes/current'))
+app.use('/forecast', require('./routes/forecast'))
+
+app.use(cors())
+
+app.listen(PORT, () => console.log('Server is running on port '+PORT))
